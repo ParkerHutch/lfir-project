658f5ee92a1d0a9887b1f4860a26e643842a42bb
Brooke Hewitt <dnj6xk@virginia.edu>
2024-11-05 21:57:59 -0500

    Added ppo files

--- cinammon_trainer.py
+++ cinammon_trainer.py
@@ -10,24 +10,30 @@ from mani_skill.utils.wrappers.record import RecordEpisode
 
 from mani_skill.vector.wrappers.sb3 import ManiSkillSB3VectorEnv
 
-environment_name = 'AnymalC-Reach-v1' # @Brooke - I used this instead of AnymalC-Move-v1 because I couldn't find the latter on the Maniskill website. Maybe that's an outdated version of the task?
+# from reach_task import AnymalCReachEnv
+# from reach_task import QuadrupedReachEnv
+
+environment_name = 'AnymalC-Reach-v1'
 def main():
-    ms3_vec_env = gym.make(environment_name, num_envs=64)
+    ms3_vec_env = gym.make(environment_name, num_envs=1024)
     max_episode_steps = gym_utils.find_max_episode_steps_value(ms3_vec_env)
     vec_env = ManiSkillSB3VectorEnv(ms3_vec_env)
 
-    model = PPO("MlpPolicy", vec_env, gamma=0.8, gae_lambda=0.9, n_steps=50, batch_size=128, n_epochs=8, verbose=1)
-    model.learn(total_timesteps=500_000)
+    model = PPO("MlpPolicy", vec_env, gamma=0.99, gae_lambda=0.95, n_steps=200, batch_size=32, n_epochs=8, verbose=1)
+    print("Starting training")
+    model.learn(total_timesteps=25_000_000)
     model.save("ppo_walk")
     vec_env.close()
+    print("Finished training")
     del model # remove to demonstrate saving and loading
 
     model = PPO.load("ppo_walk")
-
+    
     eval_vec_env = gym.make(environment_name, num_envs=16, render_mode="rgb_array")
     eval_vec_env = RecordEpisode(eval_vec_env, output_dir="eval_videos", save_video=True, trajectory_name="eval_trajectory", max_steps_per_video=max_episode_steps)
     eval_vec_env = ManiSkillSB3VectorEnv(eval_vec_env)
     obs = eval_vec_env.reset()
+    print("Now testing")
     for i in range(max_episode_steps):
         action, _states = model.predict(obs, deterministic=True)
         obs, rewards, dones, info = eval_vec_env.step(action)
